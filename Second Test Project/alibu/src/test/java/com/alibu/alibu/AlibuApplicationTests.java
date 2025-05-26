39cfd1a5d61dbfcb34e10515bde5a3a7ece1bd02
shihab-sani <mdsani633@gmail.com>
2025-05-26 23:34:34 +0600

    new test project

---
+++ Second Test Project/alibu/src/test/java/com/alibu/alibu/AlibuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alibu.alibu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlibuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
