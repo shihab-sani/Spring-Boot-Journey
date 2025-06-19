4c3dc3f3b0027ebb96760315168872e79d27eee1
shihab-sani <mdsani633@gmail.com>
2025-06-20 01:31:55 +0600

    nothing

--- First Project/testing/src/main/java/testproject/store/testing/oderService.java
+++ First Project/testing/src/main/java/testproject/store/testing/oderService.java
@@ -1,13 +1,16 @@
 package testproject.store.testing;
 
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class oderService {
     private final paymentService paymentService;
 
-    public oderService(@Qualifier("bkash") paymentService paymentService) {
+    // public oderService(@Qualifier("bkash") paymentService paymentService) {
+    //     this.paymentService = paymentService;
+    // }
+
+    public oderService(paymentService paymentService) {
         this.paymentService = paymentService;
     }
 
