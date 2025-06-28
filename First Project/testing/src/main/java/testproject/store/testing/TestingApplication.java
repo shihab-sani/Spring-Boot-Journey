a7e740c8cf7c82d3dae02a186da8c41e99bacf65
shihab-sani <mdsani633@gmail.com>
2025-06-28 23:20:21 +0600

    tested

--- First Project/testing/src/main/java/testproject/store/testing/TestingApplication.java
+++ First Project/testing/src/main/java/testproject/store/testing/TestingApplication.java
@@ -9,14 +9,14 @@ public class TestingApplication {
 
 	public static void main(String[] args) {
 		ApplicationContext context = SpringApplication.run(TestingApplication.class, args);
-		context.getBean(oderService.class);
+		// context.getBean(oderService.class);
 
-		var oderService = context.getBean(oderService.class);
-		oderService.placeOrder();
+		// var oderService = context.getBean(oderService.class);
+		// oderService.placeOrder();
 
-		var UserService = context.getBean(UserService.class);
-		UserService.registerUser(new User("Shihab","mdsani633@gmail.com",2320590,"1234567890"));
-		UserService.registerUser(new User("Sani","ssani@kent.edu",2320590,"1234567890"));
+		// var UserService = context.getBean(UserService.class);
+		// UserService.registerUser(new User("Shihab","mdsani633@gmail.com",2320590,"1234567890"));
+		// UserService.registerUser(new User("Sani","ssani@kent.edu",2320590,"1234567890"));
 
 		// var notificationManager = context.getBean(NotificationManager.class);
 		// notificationManager.sendNotification("Order placed successfully!");
