c258e725e0ed4269723e612507f620f11a6957b7
shihab-sani <mdsani633@gmail.com>
2025-07-11 00:13:59 +0600

    tested lazy fetch with profile and address entity

--- First Project/testing/src/main/java/testproject/store/testing/Services/UserService.java
+++ First Project/testing/src/main/java/testproject/store/testing/Services/UserService.java
@@ -5,12 +5,16 @@ import jakarta.transaction.Transactional;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import testproject.store.testing.entities.User;
+import testproject.store.testing.repositories.AddressesRepository;
+import testproject.store.testing.repositories.ProfileRepository;
 import testproject.store.testing.repositories.UserRepository;
 
 @AllArgsConstructor
 @Service
-public class Services {
+public class UserService {
     private final UserRepository userRepository;
+    private final ProfileRepository profileRepository;
+    private final AddressesRepository addressesRepository;
     private final EntityManager entityManager;
 
     @Transactional
@@ -32,5 +36,13 @@ public class Services {
         } else {
             System.out.println("transient or detached");
         }
+    }
+
+    @Transactional
+    public void showRelatedEntities() {
+//        var profile = profileRepository.findById(1L).orElseThrow();
+//        System.out.println(profile.getBio());
+        var address = addressesRepository.findById(1L).orElseThrow();
+        System.out.println(address.getStreet());
     }
 }
