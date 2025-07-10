e7dd0602991de6cc9d43cd79cc0130a492838082
shihab-sani <mdsani633@gmail.com>
2025-07-11 00:13:20 +0600

    tested fetched entity

--- First Project/testing/src/main/java/testproject/store/testing/TestingApplication.java
+++ First Project/testing/src/main/java/testproject/store/testing/TestingApplication.java
@@ -3,21 +3,24 @@ package testproject.store.testing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
-import testproject.store.testing.entities.Addresses;
-import testproject.store.testing.entities.Profile;
-import testproject.store.testing.entities.User;
+import testproject.store.testing.Services.UserService;
 
 @SpringBootApplication
 public class TestingApplication {
 
 	public static void main(String[] args) {
-		//ApplicationContext context = SpringApplication.run(TestingApplication.class, args);
+		ApplicationContext context = SpringApplication.run(TestingApplication.class, args);
 
-		var user = User.builder()
-				.name("John Doe")
-				.email("ssani@kent.edu")
-				.password("password123")
-				.build();
+		var service = context.getBean(UserService.class);
+
+		service.showRelatedEntities();
+
+//		var user = User.builder()
+//				.name("John Doe")
+//				.email("ssani@kent.edu")
+//				.password("password123")
+//				.build();
+//		repository.save(user);
 
 //		var addresses = Addresses.builder()
 //				.street("123 Main St")
@@ -29,13 +32,12 @@ public class TestingApplication {
 
 //		user.addTag("admin");
 
-		var profile = Profile.builder()
-						.bio("Software Engineer")
-				        .build();
-
-		user.setProfile(profile);
-		profile.setUser(user);
-
-		System.out.println(user);
+//		var profile = Profile.builder()
+//						.bio("Software Engineer")
+//				        .build();
+//
+//		user.setProfile(profile);
+//		profile.setUser(user);
+//		System.out.println(user);
 	}
 }
