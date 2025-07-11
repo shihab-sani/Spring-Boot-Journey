5c2080b1e3e6ddb3f49c076b16ae6e49cab0a8c6
shihab-sani <mdsani633@gmail.com>
2025-07-11 20:51:04 +0600

    created two new methods

--- First Project/testing/src/main/java/testproject/store/testing/Services/UserService.java
+++ First Project/testing/src/main/java/testproject/store/testing/Services/UserService.java
@@ -4,6 +4,7 @@ import jakarta.persistence.EntityManager;
 import jakarta.transaction.Transactional;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import testproject.store.testing.entities.Addresses;
 import testproject.store.testing.entities.User;
 import testproject.store.testing.repositories.AddressesRepository;
 import testproject.store.testing.repositories.ProfileRepository;
@@ -44,5 +45,25 @@ public class UserService {
 //        System.out.println(profile.getBio());
         var address = addressesRepository.findById(1L).orElseThrow();
         System.out.println(address.getStreet());
+    }
+
+    public void persistEntity() {
+        var user = User.builder().name("Sani")
+                .email("mdsani633@gmail.com")
+                .password("shihab14032001").build();
+
+        var address = Addresses.builder().street("123 Main St").city("Anytown").zip(12345).build();
+
+        user.addAddress(address);
+        userRepository.save(user);
+    }
+
+    @Transactional
+    public void deleteEntity() {
+//        userRepository.deleteById(1L);
+        var user = userRepository.findById(6L).orElseThrow();
+        var address = user.getAddresses().getFirst();
+        user.removeAddress(address);
+        userRepository.save(user);
     }
 }
