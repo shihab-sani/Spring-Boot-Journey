0948df38312bd63236c72acb3baef400a13b1853
shihab-sani <mdsani633@gmail.com>
2025-07-14 18:40:52 +0600

    added new methods for custom queries with SQL and JPQL

--- First Project/testing/src/main/java/testproject/store/testing/repositories/ProductsRepository.java
+++ First Project/testing/src/main/java/testproject/store/testing/repositories/ProductsRepository.java
@@ -1,7 +1,36 @@
 package testproject.store.testing.repositories;
 
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import testproject.store.testing.entities.Products;
 
+import java.math.BigDecimal;
+import java.util.List;
+
 public interface ProductsRepository extends CrudRepository<Products, Long> {
+    //Sort(By Order)
+    List<Products> findByNameOrderByPrice(String name);
+
+    //Limit(Top/First)
+    List<Products> findTop3ByNameOrderByPrice(String name);
+    List<Products> findFirst3ByNameLikeOrderByPrice(String name);
+
+//    List<Products> findByPriceBetweenOrderByName(BigDecimal min, BigDecimal max);
+
+    //With SQL commands
+//    @Query(value = "SELECT * FROM products p WHERE p.price BETWEEN :min AND :max ORDER BY p.name", nativeQuery = true)
+//    List<Products> findByPrice(@Param("min") BigDecimal min, @Param("max") BigDecimal max);
+
+    //With JPQL
+    @Query(value = "SELECT p FROM Products p JOIN p.category WHERE p.price BETWEEN :min AND :max ORDER BY p.name")
+    List<Products> findProducts(@Param("min") BigDecimal min, @Param("max") BigDecimal max);
+
+    @Query(value = "SELECT COUNT(*) FROM Products p WHERE p.price BETWEEN :min AND :max")
+    long countProducts(@Param("min") BigDecimal min, @Param("max") BigDecimal max);
+
+    @Modifying
+    @Query(value = "UPDATE Products p SET p.price = :newPrice WHERE p.category.id = :catalogId")
+    void updatePrice(BigDecimal newPrice, Byte catalogId);
 }
