41dad996ec2908caf8c4085c39752674b63ec381
shihab-sani <mdsani633@gmail.com>
2025-07-16 22:01:13 +0600

    created two more methods for data user fetching

--- First Project/testing/src/main/java/testproject/store/testing/repositories/UserRepository.java
+++ First Project/testing/src/main/java/testproject/store/testing/repositories/UserRepository.java
@@ -1,7 +1,18 @@
 package testproject.store.testing.repositories;
 
+import org.springframework.data.jpa.repository.EntityGraph;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import testproject.store.testing.entities.User;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface UserRepository extends CrudRepository<User, Long> {
+    @EntityGraph(attributePaths = {"tags","addresses"})
+    Optional<User> findByEmail(String email);
+
+    @EntityGraph(attributePaths = {"addresses"})
+    @Query("select u from User u")
+    List<User> findAllWithAddress();
 }
