ef8b6aff33b441f905c05af548408a2e0d98ac85
shihab-sani <mdsani633@gmail.com>
2025-07-18 22:04:08 +0600

    exchange methods between user and profile repository

--- First Project/testing/src/main/java/testproject/store/testing/repositories/ProfileRepository.java
+++ First Project/testing/src/main/java/testproject/store/testing/repositories/ProfileRepository.java
@@ -1,11 +1,23 @@
 package testproject.store.testing.repositories;
 
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
+import testproject.store.testing.DataTransferObjects.UserSummary;
 import testproject.store.testing.entities.Profile;
 
+import java.util.List;
+
 public interface ProfileRepository extends CrudRepository<Profile, Long> {
-    @Query(value = "SELECT p.id, p.bio, p.phoneNumber, p.dateOfBirth, p.loyaltyPoints FROM Profile p WHERE p.loyaltyPoints > :value")
-    long findByLoyaltyPoints(@Param("value") int value);
+//    @Query(value = "SELECT p.id, p.bio, p.phoneNumber, p.dateOfBirth, p.loyaltyPoints FROM Profile p WHERE p.loyaltyPoints > :value")
+//    List<Profile> findByLoyaltyPoints(@Param("value") Integer value);
+//
+//    @Query("select p from Profile p where p.loyaltyPoints > :value")
+//    @EntityGraph(attributePaths = {"user"})
+//    List<Profile> findLoyaltyPoint(@Param("value") Integer value);
+//
+//    @Query("select p.id as id, p.user.email as email from Profile p where p.loyaltyPoints > :value order by p.user.email")
+//    @EntityGraph(attributePaths = {"profile"})
+//    List<UserSummary> findLoyaltyPointsByOrder(@Param("value") Integer value);
 }
--- First Project/testing/src/main/java/testproject/store/testing/repositories/UserRepository.java
+++ First Project/testing/src/main/java/testproject/store/testing/repositories/UserRepository.java
@@ -3,6 +3,8 @@ package testproject.store.testing.repositories;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import testproject.store.testing.DataTransferObjects.UserSummary;
 import testproject.store.testing.entities.User;
 
 import java.util.List;
@@ -15,4 +17,7 @@ public interface UserRepository extends CrudRepository<User, Long> {
     @EntityGraph(attributePaths = {"addresses"})
     @Query("select u from User u")
     List<User> findAllWithAddress();
+
+    @Query("select u.id as id, u.email as email from User u where u.profile.loyaltyPoints > :value order by u.email")
+    List<UserSummary> findLoyalUser(@Param("value") Integer value);
 }
