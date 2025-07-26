d7af08a4f191058e22356c244abbecf3ed5790a8
shihab-sani <mdsani633@gmail.com>
2025-07-26 23:34:08 +0600

    imported database from first project

---
+++ E-Commerce Application/src/main/resources/db/migration/V2__profile_table_added.sql
@@ -0,0 +1,14 @@
+create table profile
+(
+    id            bigint auto_increment,
+    bio           varchar(255)   not null,
+    phone_number  bigint         null,
+    date_of_birth varchar(255)   null,
+    loyalty_point int default 10 null,
+    constraint profile_user_id_fk
+        foreign key (id) references user (id)
+);
+
+alter table profile
+    modify id bigint auto_increment;
+
---
+++ E-Commerce Application/src/main/resources/db/migration/V4__profile_date_colume.sql
@@ -0,0 +1,2 @@
+alter table profile
+    modify date_of_birth date null;
