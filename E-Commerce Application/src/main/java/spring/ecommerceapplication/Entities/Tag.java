6590dacb00095b7899ba80204ffbf8e195242224
shihab-sani <mdsani633@gmail.com>
2025-07-26 23:34:41 +0600

    imported entity from first project

---
+++ E-Commerce Application/src/main/java/spring/ecommerceapplication/Entities/Tag.java
@@ -0,0 +1,27 @@
+package spring.ecommerceapplication.Entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "tags")
+public class Tag {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private long id;
+
+   @Column(name = "name")
+    private String name;
+
+   @ManyToMany(mappedBy = "tags")
+   @ToString.Exclude
+   private Set<User> users = new HashSet<>();
+}
