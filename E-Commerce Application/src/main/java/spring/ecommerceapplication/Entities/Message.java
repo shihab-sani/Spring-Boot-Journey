590cf4002ea35f81001f3fd19db678f50a0770a2
shihab-sani <mdsani633@gmail.com>
2025-07-27 20:47:32 +0600

    created a new message class for sent message

---
+++ E-Commerce Application/src/main/java/spring/ecommerceapplication/Entities/Message.java
@@ -0,0 +1,10 @@
+package spring.ecommerceapplication.Entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class Message {
+    private String message;
+}
