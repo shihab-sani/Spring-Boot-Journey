52a47fa27a42348868f29172ab51d18d61d9dbb1
shihab-sani <mdsani633@gmail.com>
2025-07-27 20:48:45 +0600

    using thymeleaf generated name

--- E-Commerce Application/src/main/java/spring/ecommerceapplication/Controller/HomeController.java
+++ E-Commerce Application/src/main/java/spring/ecommerceapplication/Controller/HomeController.java
@@ -1,17 +1,19 @@
 package spring.ecommerceapplication.Controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
     @RequestMapping("/")
-    public String index() {
-        return "index.html";
+    public String index(Model model) {
+        model.addAttribute("name", "Shihab");
+        return "index";
     }
 
-    @RequestMapping("/hello")
-    public String sayHello() {
-        return "index.html";
-    }
+//    @RequestMapping("/hello")
+//    public String sayHello() {
+//        return "index.html";
+//    }
 }
