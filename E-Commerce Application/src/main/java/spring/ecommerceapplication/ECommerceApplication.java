891c412e5c930c6418ed52c8e85dc2a7bfb756dd
shihab-sani <mdsani633@gmail.com>
2025-07-29 23:24:40 +0600

    tested the functionality

--- E-Commerce Application/src/main/java/spring/ecommerceapplication/ECommerceApplication.java
+++ E-Commerce Application/src/main/java/spring/ecommerceapplication/ECommerceApplication.java
@@ -2,12 +2,16 @@ package spring.ecommerceapplication;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import spring.ecommerceapplication.Controller.HomeController;
 
 @SpringBootApplication
 public class ECommerceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ECommerceApplication.class, args);
+        ApplicationContext context = SpringApplication.run(ECommerceApplication.class, args);
+
+        var beanNames = context.getBean(HomeController.class);
     }
 
 }
