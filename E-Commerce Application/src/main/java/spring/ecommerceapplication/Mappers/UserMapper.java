342a855f4b8420e02501e8b24bbf827a0e04a23e
shihab-sani <mdsani633@gmail.com>
2025-07-30 22:13:45 +0600

    created userMapper for mapping user data

---
+++ E-Commerce Application/src/main/java/spring/ecommerceapplication/Mappers/UserMapper.java
@@ -0,0 +1,12 @@
+package spring.ecommerceapplication.Mappers;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import spring.ecommerceapplication.DTOS.UserDtos;
+import spring.ecommerceapplication.Entities.User;
+
+@Mapper(componentModel = "spring")
+public interface UserMapper {
+    @Mapping(target = "createdAt", expression = "java(java.time.LocalDateTime.now())")
+    UserDtos toDto(User user);
+}
