558d62e3af9dae1186e7aad597099c51e87de9bd
shihab-sani <mdsani633@gmail.com>
2025-07-30 22:12:31 +0600

    unnecessary methods deleted

--- E-Commerce Application/src/main/java/spring/ecommerceapplication/Repositories/UserRepository.java
+++ E-Commerce Application/src/main/java/spring/ecommerceapplication/Repositories/UserRepository.java
@@ -1,6 +1,7 @@
 package spring.ecommerceapplication.Repositories;
 
 import org.springframework.data.jpa.repository.EntityGraph;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
@@ -10,12 +11,5 @@ import spring.ecommerceapplication.Entities.User;
 import java.util.List;
 import java.util.Optional;
 
-public interface UserRepository extends CrudRepository<User, Long> {
-    @EntityGraph(attributePaths = {"tags","addresses"})
-    Optional<User> findByEmail(String email);
-
-    @EntityGraph(attributePaths = {"addresses"})
-    @Query("select u from User u")
-    List<User> findAllWithAddress();
-
+public interface UserRepository extends JpaRepository<User, Long> {
 }
