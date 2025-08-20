4452dbd10f1b409a3765503e19830322deaea49e
shihab-sani <mdsani633@gmail.com>
2025-08-20 23:34:58 +0600

    added JPA entity classes for User and Profile with relationships and schema mappings

---
+++ ECommerce Appication/ECommerce Application/src/main/java/spring/ecommerceapplication/Entities/User.java
@@ -0,0 +1,38 @@
+package spring.ecommerceapplication.Entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "user", schema = "ecommerce")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+
+    @OneToMany(mappedBy = "user", cascade = {CascadeType.PERSIST, CascadeType.REMOVE}, orphanRemoval = true)
+    private Set<Address> addresses = new LinkedHashSet<>();
+
+    @OneToOne(mappedBy = "user", cascade = CascadeType.REMOVE)
+    private Profile profile;
+
+    @ManyToMany
+    private Set<Product> products = new LinkedHashSet<>();
+
+}
