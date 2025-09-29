9a296197221e932f8c60f864afc4603184db3fe9
shihab-sani <mdsani633@gmail.com>
2025-09-29 22:54:31 +0600

    updated Cart entity with eager fetching for cartItems and added method to calculate total price; renamed field in CartProductDtos

--- Second Project/src/main/java/spring/secondproject/DTOS/CartProductDtos.java
+++ Second Project/src/main/java/spring/secondproject/DTOS/CartProductDtos.java
@@ -7,6 +7,6 @@ import java.math.BigDecimal;
 @Data
 public class CartProductDtos {
     private Long id;
-    private String productName;
+    private String name;
     private BigDecimal price;
 }
--- Second Project/src/main/java/spring/secondproject/Entities/Cart.java
+++ Second Project/src/main/java/spring/secondproject/Entities/Cart.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.LinkedHashSet;
 import java.util.Set;
@@ -22,7 +23,13 @@ public class Cart {
     @Column(name = "date", updatable = false, insertable = false)
     private LocalDate date;
 
-    @OneToMany(mappedBy = "cart", cascade = CascadeType.MERGE)
+    @OneToMany(mappedBy = "cart", cascade = CascadeType.MERGE, fetch = FetchType.EAGER)
     private Set<CartItem> cartItems = new LinkedHashSet<>();
+
+    public BigDecimal getTotalPrice() {
+        return cartItems.stream()
+                .map(CartItem::getPrice).
+                reduce(BigDecimal.ZERO, BigDecimal::add);
+    }
 
 }
