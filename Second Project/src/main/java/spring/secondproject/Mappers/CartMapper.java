1067a98eb520badb686c38d6ba70a6c003b7e5b7
shihab-sani <mdsani633@gmail.com>
2025-09-29 22:54:59 +0600

    updated CartRepository with custom query and entity graph for fetching cart with items; enhanced CartMapper to map cartItems and calculate total price

--- Second Project/src/main/java/spring/secondproject/Mappers/CartMapper.java
+++ Second Project/src/main/java/spring/secondproject/Mappers/CartMapper.java
@@ -9,7 +9,10 @@ import spring.secondproject.Entities.CartItem;
 
 @Mapper(componentModel = "spring")
 public interface CartMapper {
+    @Mapping(target = "items", source = "cartItems")
+    @Mapping(target = "totalPrice", expression = "java(cart.getTotalPrice())")
     CartDtos toDtos(Cart cart);
+
     @Mapping(target = "totalPrice", expression = "java(cartItem.getPrice())")
     CartItemsDtos toDtos(CartItem cartItem);
 }
--- Second Project/src/main/java/spring/secondproject/Repositories/CartRepository.java
+++ Second Project/src/main/java/spring/secondproject/Repositories/CartRepository.java
@@ -1,11 +1,16 @@
 package spring.secondproject.Repositories;
 
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import spring.secondproject.Entities.Cart;
 
 import java.util.Optional;
 import java.util.UUID;
 
-public interface CartRepository  extends JpaRepository<Cart, Long> {
-    Optional<Cart> findById(UUID cartId);
+public interface CartRepository  extends JpaRepository<Cart, UUID> {
+    @EntityGraph(attributePaths = "cartItems.product")
+    @Query("SELECT c FROM Cart c WHERE c.id = :cartId")
+    Optional<Cart> getCartWithItems(@Param("cartId") UUID cartId);
 }
