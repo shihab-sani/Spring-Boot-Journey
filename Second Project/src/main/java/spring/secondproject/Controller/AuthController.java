afa1ba14980ad768cb9bc1f50dea8ccadbf81bee
shihab-sani <mdsani633@gmail.com>
2025-10-02 21:38:09 +0600

    refactored AuthController to use AuthenticationManager for login authentication; replaced manual password validation with Spring Security authentication mechanism and added exception handler for BadCredentialsException

--- Second Project/src/main/java/spring/secondproject/Controller/AuthController.java
+++ Second Project/src/main/java/spring/secondproject/Controller/AuthController.java
@@ -4,31 +4,26 @@ import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.web.bind.annotation.*;
 import spring.secondproject.DTOS.LogInRequest;
-import spring.secondproject.Repositories.UserRepository;
 
 @RestController
 @RequestMapping("/auth")
 @AllArgsConstructor
 public class AuthController {
-    private final PasswordEncoder passwordEncoder;
-    private UserRepository userRepository;
+    private final AuthenticationManager authenticationManager;
 
     @PostMapping("/login")
     public ResponseEntity<Void> logIn(@Valid @RequestBody LogInRequest request) {
-        var user = userRepository.findByEmail(request.getEmail()).orElse(null);
-        if (user == null) {
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
-        }
-
-        if (!passwordEncoder.matches(request.getPassword(), user.getPassword())) {
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
-        }
+        authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(request.getEmail(), request.getPassword()));
         return ResponseEntity.ok().build();
+    }
+
+    @ExceptionHandler(BadCredentialsException.class)
+    public ResponseEntity<Void> handlerBadCredentialsException() {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
     }
 }
