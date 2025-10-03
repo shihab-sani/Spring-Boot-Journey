3f8420b23d3f6f1e4fe2f884c7f92c749432e238
shihab-sani <mdsani633@gmail.com>
2025-10-03 23:31:35 +0600

    updated JwtServices to read JWT secret from application properties using @Value annotation

--- Second Project/src/main/java/spring/secondproject/Services/JwtServices.java
+++ Second Project/src/main/java/spring/secondproject/Services/JwtServices.java
@@ -2,19 +2,23 @@ package spring.secondproject.Services;
 
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
 
 @Service
 public class JwtServices {
+    @Value( "${spring.jwt.secret}")
+    private String secret;
+
     public String generateJwtToken(String email) {
         final long tokenExpiration = 86400; //1 day
         return Jwts.builder()
                 .subject(email)
                 .issuedAt(new Date())
                 .expiration(new Date(System.currentTimeMillis() + tokenExpiration * 1000))
-                .signWith(Keys.hmacShaKeyFor("secret".getBytes()))
+                .signWith(Keys.hmacShaKeyFor(secret.getBytes()))
                 .compact();
     }
 }
