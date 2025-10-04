6dd34b2cbae67979bec9bc393978411c798eb447
shihab-sani <mdsani633@gmail.com>
2025-10-04 22:25:41 +0600

    added JWT token validation in JwtServices and updated SecurityConfig to allow access to validation endpoint

--- Second Project/src/main/java/spring/secondproject/Services/JwtServices.java
+++ Second Project/src/main/java/spring/secondproject/Services/JwtServices.java
@@ -1,11 +1,13 @@
 package spring.secondproject.Services;
 
+import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
+import java.util.jar.JarException;
 
 @Service
 public class JwtServices {
@@ -20,5 +22,17 @@ public class JwtServices {
                 .expiration(new Date(System.currentTimeMillis() + tokenExpiration * 1000))
                 .signWith(Keys.hmacShaKeyFor(secret.getBytes()))
                 .compact();
+    }
+
+    public boolean validateJwtToken(String authToken) {
+        try {
+            var claims = Jwts.parser()
+                    .verifyWith(Keys.hmacShaKeyFor(secret.getBytes()))
+                    .build().parseSignedClaims(authToken)
+                    .getPayload();
+            return claims.getExpiration().after(new Date());
+        } catch (JwtException ex) {
+            return false;
+        }
     }
 }
