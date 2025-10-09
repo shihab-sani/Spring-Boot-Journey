c3aff8e9d2b37df2634e5cbd857298ebcf5315f5
shihab-sani <mdsani633@gmail.com>
2025-10-09 23:36:37 +0600

    restricted access to "/admin/**" endpoints to ADMIN role; added access denied handler returning 403 status

--- Second Project/src/main/java/spring/secondproject/Config/SecurityConfig.java
+++ Second Project/src/main/java/spring/secondproject/Config/SecurityConfig.java
@@ -19,6 +19,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import spring.secondproject.Entities.Role;
 import spring.secondproject.Filter.JwtAuthenticationFilter;
 
 @Configuration
@@ -52,13 +53,18 @@ public class SecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(c-> c
                         .requestMatchers("cart/**").permitAll()
+                        .requestMatchers("/admin/**").hasRole(Role.ADMIN.name())
                         .requestMatchers(HttpMethod.POST,"/users").permitAll()
                         .requestMatchers(HttpMethod.POST,"/auth/login").permitAll()
                         .requestMatchers(HttpMethod.POST,"/auth/refresh").permitAll()
                         .anyRequest().authenticated())
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
-                .exceptionHandling(c->
-                        c.authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED)));
+                .exceptionHandling(c->{
+                        c.authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED));
+                        c.accessDeniedHandler((request, response, accessDeniedException) -> {
+                            response.setStatus(HttpStatus.FORBIDDEN.value());
+                        });
+                        });
         return http.build();
     }
 }
