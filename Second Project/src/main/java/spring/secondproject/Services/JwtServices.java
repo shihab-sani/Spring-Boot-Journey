b484c0b6c88bc5a87e78d8ffd658b82c4d99f1d5
shihab-sani <mdsani633@gmail.com>
2025-10-10 00:30:43 +0600

    refactored JwtServices to use Jwt class for token generation and parsing; removed redundant methods for extracting claims and role

--- Second Project/src/main/java/spring/secondproject/Services/JwtServices.java
+++ Second Project/src/main/java/spring/secondproject/Services/JwtServices.java
@@ -1,14 +1,10 @@
 package spring.secondproject.Services;
 
 import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.security.Keys;
 import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import spring.secondproject.Config.JwtConfig;
-import spring.secondproject.Entities.Role;
 import spring.secondproject.Entities.User;
 
 import java.util.Date;
@@ -19,32 +15,32 @@ public class JwtServices {
     private final JwtConfig jwtConfig;
 
 
-    public String generateAccessToken(User user) {
+    public Jwt generateAccessToken(User user) {
         return generateToken(user, jwtConfig.getAccessTokenExpiration());
     }
 
-    public String generateRefreshToken(User user) {
+    public Jwt generateRefreshToken(User user) {
         return generateToken(user, jwtConfig.getRefreshTokenExpiration());
     }
 
-    private String generateToken(User user, long tokenExpiration) {
-        return Jwts.builder()
+    private Jwt generateToken(User user, long tokenExpiration) {
+        var claims = Jwts.claims()
                 .subject(String.valueOf(user.getId()))
-                .claim("email", user.getEmail())
-                .claim("name", user.getName())
-                .claim("role", user.getRole())
+                .add("email", user.getEmail())
+                .add("name", user.getName())
+                .add("role" , user.getRole())
                 .issuedAt(new Date())
                 .expiration(new Date(System.currentTimeMillis() + tokenExpiration * 1000))
-                .signWith(jwtConfig.getSecretKey())
-                .compact();
+                .build();
+        return new Jwt(claims, jwtConfig.getSecretKey());
     }
 
-    public boolean validateJwtToken(String authToken) {
+    public Jwt parseToken(String authToken) {
         try {
             var claims = getClaims(authToken);
-            return claims.getExpiration().after(new Date());
-        } catch (JwtException ex) {
-            return false;
+            return new Jwt(claims, jwtConfig.getSecretKey());
+        } catch (Exception e) {
+            return null;
         }
     }
 
@@ -53,13 +49,5 @@ public class JwtServices {
                 .verifyWith(jwtConfig.getSecretKey())
                 .build().parseSignedClaims(authToken)
                 .getPayload();
-    }
-
-    public Long getUserIdFromJwtToken(String token) {
-        return Long.valueOf(getClaims(token).getSubject());
-    }
-
-    public Role getRoleFromJwtToken(String token) {
-        return Role.valueOf(getClaims(token).get("role", String.class));
     }
 }
