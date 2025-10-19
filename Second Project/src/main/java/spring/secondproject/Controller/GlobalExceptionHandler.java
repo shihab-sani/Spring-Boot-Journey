52de704080a0278c1cdffdd7dafb5fdfd2c39cca
shihab-sani <mdsani633@gmail.com>
2025-10-19 23:42:34 +0600

    added exception handler for HttpMessageNotReadableException to handle invalid request bodies and return structured error responses

--- Second Project/src/main/java/spring/secondproject/Controller/GlobalExceptionHandler.java
+++ Second Project/src/main/java/spring/secondproject/Controller/GlobalExceptionHandler.java
@@ -1,15 +1,22 @@
 package spring.secondproject.Controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import spring.secondproject.DTOS.ErrorDtos;
 
 import java.util.HashMap;
 import java.util.Map;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<ErrorDtos> handleUnreadableMessageException() {
+        return ResponseEntity.badRequest().body(new ErrorDtos("Invalid request body"));
+    }
+
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<Map<String,String>> handleValidationExceptions(MethodArgumentNotValidException exception ) {
         var errors = new HashMap<String,String>();
